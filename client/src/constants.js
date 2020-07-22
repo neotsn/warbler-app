93bb71c4c514678c9ada7fbc23f7356f6e804f9a
neotsn neotsn <neotsn@gmail.com>
2020-07-22 10:51:36 -0500

    Add DB Tables and Fields constants; reorder a-z

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,18 +1,29 @@
 /**
- * Application URLs
- */
-exports.URLS = {
-  API_SERVER: 'http://127.0.0.1:3100',
-  CLIENT: 'http://127.0.0.1:3000'
-};
-
-/**
  * Export the Warbler Server API Endpoint constants
  */
 exports.API_ENDPOINTS = {
   TWITTER_AUTH: '/twitter',
   TWITTER_AUTH_CALLBACK: '/twitter/callback',
-  TWITTER_GET_PROFILE: '/twitter/profile/get'
+  TWITTER_GET_USER: '/twitter/user/get'
+};
+
+/**
+ * Export the field sets for each of the DB table names
+ * @type {{TWITTER_TOKENS: {OAUTH_VERIFIER: string, OAUTH_TOKEN: string, OAUTH_TOKEN_SECRET: string}}}
+ */
+exports.DB_FIELDS = {
+  TWITTER_TOKENS: {
+    OAUTH_TOKEN: 'oauth_token',
+    OAUTH_TOKEN_SECRET: 'oauth_token_secret',
+    OAUTH_VERIFIER: 'oauth_verifier'
+  }
+};
+
+/**
+ * Export the LocalStorage Database Table names
+ */
+exports.DB_TABLES = {
+  TWITTER_TOKENS: 'warbler.twitter.tokens'
 };
 
 /**
@@ -20,6 +31,14 @@ exports.API_ENDPOINTS = {
  */
 exports.SOCKET_EVENTS = {
   ERROR: 'warbler.error',
-  TWITTER_USER: 'warbler.twitter.user',
-  TWITTER_GET_PROFILE: 'warbler.twitter.get.profile'
+  TWITTER_AUTH: 'warbler.twitter.auth',
+  TWITTER_GET_USER: 'warbler.twitter.get.user'
+};
+
+/**
+ * Application URLs
+ */
+exports.URLS = {
+  API_SERVER: 'http://127.0.0.1:3100',
+  CLIENT: 'http://127.0.0.1:3000'
 };
