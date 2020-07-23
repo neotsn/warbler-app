32ca97e0091a74de080e81567dfd37d5f34c7e88
neotsn neotsn <neotsn@gmail.com>
2020-07-23 14:28:01 -0500

    Add a left-navigation drawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, { Fragment } from 'react';
-import { createMuiTheme, CssBaseline, ThemeProvider, withStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, Divider, List, ThemeProvider, Toolbar, withStyles } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
 import { Route } from 'react-router-dom';
 import { deepPurple, lightBlue } from '@material-ui/core/colors';
 import AppHeader from './components/AppHeader';
 import Home from './pages/Home';
+import ViewStream from '@material-ui/icons/ViewStream';
+import Settings from '@material-ui/icons/Settings';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
 
 // Setup the custom colors
 const theme = createMuiTheme({
@@ -19,7 +27,21 @@ const theme = createMuiTheme({
 
 // Add some styles using the theme settings
 const styles = theme => ({
-  main: {
+  root: {
+    display: 'flex'
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerContainer: {
+    overflow: 'auto'
+  },
+  content: {
+    flexGrow: 1,
     padding: theme.spacing(3),
     [theme.breakpoints.down('xs')]: {
       padding: theme.spacing(2)
@@ -31,13 +53,44 @@ const styles = theme => ({
 const App = ({ classes }) => {
   return (
     <Fragment>
-      <CssBaseline/>
-      <ThemeProvider theme={theme}>
-        <AppHeader/>
-        <main className={classes.main}>
-          <Route exact path={'/'} component={Home}/>
-        </main>
-      </ThemeProvider>
+      <div className={classes.root}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <AppHeader/>
+          <Drawer
+            className={classes.drawer}
+            variant={'permanent'}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            <Toolbar/>
+            <div className={classes.drawerContainer}>
+              <List>
+                <ListItem button key={'Feed'}>
+                  <ListItemIcon><ViewStream/></ListItemIcon>
+                  <ListItemText primary={'Feed'}/>
+                </ListItem>
+                <ListItem button key={'Settings'}>
+                  <ListItemIcon><Settings/></ListItemIcon>
+                  <ListItemText primary={'Settings'}/>
+                </ListItem>
+              </List>
+              <Divider/>
+              <List>
+                <ListItem button key={'Logout'}>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={'Logout'}/>
+                </ListItem>
+              </List>
+            </div>
+          </Drawer>
+          <main className={classes.content}>
+            <Toolbar/>
+            <Route exact path={'/'} component={Home}/>
+          </main>
+        </ThemeProvider>
+      </div>
     </Fragment>
   );
 };
--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, withStyles } from '@material-ui/core';
 import LoginButton from './LoginButton';
 
-const styles = {
+const styles = theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
   headerButtons: {
     textAlign: 'right',
     flex: 1
   }
-};
+});
 
 const AppHeader = ({ classes }) => {
   return (
-    <AppBar position={'static'}>
+    <AppBar position={'fixed'} className={classes.appBar}>
       <Toolbar>
         <Typography variant={'h6'} color={'inherit'}>
           Warbler
