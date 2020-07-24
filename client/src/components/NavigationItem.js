e2aa34471a5555834b4ef717cc6c27c868b1ec06
neotsn neotsn <neotsn@gmail.com>
2020-07-24 16:59:20 -0500

    Add a navigation item component with properties

---
+++ client/src/components/NavigationItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Link } from 'react-router-dom';
+
+/**
+ * Generate a Navigation Item consistently
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const NavigationItem = (props) => {
+  return (
+    <Link to={props.url}>
+      <ListItem button key={props.label}>
+        <ListItemIcon>{props.icon || null}</ListItemIcon>
+        <ListItemText primary={props.label}/>
+      </ListItem>
+    </Link>
+  );
+};
+
+export default NavigationItem;
