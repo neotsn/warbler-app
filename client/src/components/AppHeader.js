b9129d32100f8b1bd37b6c19d6ef87d65aa5187f
neotsn neotsn <neotsn@gmail.com>
2020-07-27 09:37:22 -0500

    Cleanup AppHeader component as a Class component

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppBar, Toolbar, Typography, withStyles } from '@material-ui/core';
 import LoginButton from './LoginButton';
 
+// Setup some styles using the theme settings
 const styles = theme => ({
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
+    zIndex: theme.zIndex.drawer + 1
   },
   headerButtons: {
     textAlign: 'right',
@@ -12,19 +13,30 @@ const styles = theme => ({
   }
 });
 
-const AppHeader = ({ classes }) => {
-  return (
-    <AppBar position={'fixed'} className={classes.appBar}>
-      <Toolbar>
-        <Typography variant={'h6'} color={'inherit'}>
-          Warbler
-        </Typography>
-        <div className={classes.headerButtons}>
-          <LoginButton/>
-        </div>
-      </Toolbar>
-    </AppBar>
-  );
+// Generate the AppHeader output
+class AppHeader extends Component {
+  constructor({ classes, props } = {}) {
+    super(props);
+
+    this.classes = classes;
+  }
+
+  render() {
+    const { appBar, headerButtons } = this.classes;
+
+    return (
+      <AppBar position={'fixed'} className={appBar}>
+        <Toolbar>
+          <Typography variant={'h6'} color={'inherit'}>
+            Warbler
+          </Typography>
+          <div className={headerButtons}>
+            <LoginButton initUser={this.props.initUser}/>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 };
 
 export default withStyles(styles)(AppHeader);
