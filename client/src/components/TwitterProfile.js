b5e8cc5557b54dd77c56784e6e7566fe44136883
neotsn neotsn <neotsn@gmail.com>
2020-07-29 15:56:38 -0500

    Add a TwitterProfile component

---
+++ client/src/components/TwitterProfile.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { Done } from '@material-ui/icons';
+
+export default class TwitterProfile extends Component {
+  constructor({ classes, props } = {}) {
+    super(props);
+
+    this.classes = classes;
+    this.state = {
+      profile: '',
+      characterCount: 0,
+      maxCharacters: 160
+    };
+  }
+
+  componentDidUpdate() {
+    this.doProfileChange();
+  }
+
+  doProfileChange() {
+    if (!this.state.profile && this.state.profile !== this.props.user.description) {
+      this.setState({
+        profile: this.props.user.description,
+        characterCount: (this.props.user.description || '').length
+      });
+    }
+  }
+
+  /**
+   * Update a Character Count as the user types
+   */
+  onInputChange = (e) => {
+    this.setState({
+      profile: e.target.value,
+      characterCount: e.target.value.length
+    });
+  };
+
+  onProfileSubmit = (e) => {
+    e.preventDefault();
+    this.props.onProfileUpdate({
+      data: {
+        description: this.state.profile
+      }
+    });
+  };
+
+  render() {
+    const { profile, characterCount, maxCharacters } = this.state;
+
+    return (
+      <div>
+        <TextField
+          label={'Twitter Profile Description'}
+          name={'twitter_user_description'}
+          rows={5}
+          rowsMax={10}
+          margin={'normal'}
+          value={profile || ''}
+          variant={'outlined'}
+          multiline
+          fullWidth={true}
+          helperText={`${characterCount}/${maxCharacters}`}
+          onChange={this.onInputChange}
+        />
+        <Button
+          variant={'contained'}
+          color={'secondary'}
+          startIcon={<Done/>}
+          onClick={this.onProfileSubmit}
+        >{'Save'}</Button>
+      </div>
+    );
+  }
+}
+
