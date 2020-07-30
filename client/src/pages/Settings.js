da7006a41f0844a16bcf04f3f2d95d32f64f8e86
neotsn neotsn <neotsn@gmail.com>
2020-07-30 16:54:53 -0500

    Iterate some styles on the Settings page, in preparation for PGP stuff

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { Divider, Typography } from '@material-ui/core';
+import { Divider, Paper, Typography, withStyles } from '@material-ui/core';
 import TwitterProfile from '../components/TwitterProfile';
+
+const styles = theme => ({
+  paper: {
+    padding: theme.spacing(1),
+    border: `1px solid ${theme.palette.divider}`
+  }
+});
 
 /**
  * The settings page to handle Key upload/generation and adjusting the Twitter Profile Description
  */
-export default class Settings extends Component {
+class Settings extends Component {
   constructor({ classes, props } = {}) {
     super(props);
 
@@ -15,15 +22,19 @@ export default class Settings extends Component {
   render() {
     return (
       <div>
-        <Typography variant={'h4'}>Settings</Typography>
-        <Divider/>
-        <br/>
-        <Typography variant={'h6'}>Twitter Settings</Typography>
-        <TwitterProfile
-          user={this.props.user}
-          onProfileUpdate={this.props.onProfileUpdate}
-        />
+        <Paper elevation={0} className={this.classes.paper}>
+          <Typography variant={'h4'}>Settings</Typography>
+          <Divider/>
+          <br/>
+          <Typography variant={'h6'}>Twitter Settings</Typography>
+          <TwitterProfile
+            user={this.props.user}
+            onProfileUpdate={this.props.onProfileUpdate}
+          />
+        </Paper>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(Settings);
