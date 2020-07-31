2ec426bfbfd2064ef59fc32cbc2fd50614e6eb11
neotsn neotsn <neotsn@gmail.com>
2020-07-31 15:55:01 -0500

    Add a TabPanel component to handle tabbed interface panels

---
+++ client/src/components/TabPanel.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Box, withStyles } from '@material-ui/core';
+
+const styles = (theme) => ({
+  tabPanel: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%'
+  },
+  tabPanelContent: {
+    width: '100%'
+  }
+});
+
+class TabPanel extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired
+  };
+
+  constructor({ classes, props }) {
+    super(props);
+
+    this.classes = classes;
+  }
+
+  render() {
+    return (
+      <div
+        className={this.classes.tabPanel}
+        role="tabpanel"
+        hidden={this.props.value !== this.props.index}
+        id={`full-width-tabpanel-${this.props.index}`}
+        aria-labelledby={`full-width-tab-${this.props.index}`}
+        {...this.props.other}
+      >
+        {this.props.value === this.props.index && (
+          <Box p={2} className={this.classes.tabPanelContent}>
+            <div>
+              {this.props.children}
+            </div>
+          </Box>
+        )}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(TabPanel);
